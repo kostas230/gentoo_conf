b17220b245ea9639196db248c8f822ee260e6420
kostas230 <57586901+kostas230@users.noreply.github.com>
2023-01-18 19:51:13 +0000

    Create install_fonts.sh

---
+++ install_fonts.sh
@@ -0,0 +1,23 @@
+emerge media-fonts/termsyn \
+  media-fonts/terminus-font \
+  media-fonts/ttf-bitstream-vera \
+  media-fonts/croscorefonts \ 
+  media-fonts/dejavu \
+  media-fonts/drod \
+  media-fonts/freefonts \
+  media-fonts/ibm-plex \
+  media-fonts/liberation-fonts \
+  media-fonts/libertine \ 
+  media-fonts/noto \
+  media-fonts/roboto \
+  media-fonts/tex-gyre \
+  media-fonts/ubuntu-font-family \
+  media-fonts/anonymous-pro \
+  media-fonts/cascadia-code \
+  media-fonts/fantasque-sans-mono \
+  media-fonts/monoid \
+  media-fonts/source-code-pro \
+  media-fonts/open-sans \
+  media-fonts/sil-gentium \
+  media-fonts/cm-unicode \
+  media-fonts/xorg-fonts 
